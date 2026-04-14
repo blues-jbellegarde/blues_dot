c3681d086a7562b2cdd7fe0eb371bdbcbc1c8732
Jason Bellegarde <jbellegarde@blues.com>
2026-04-14 15:18:45 -0600

    migrate nvim-treesitter to main branch for neovim 0.12

--- nvim/.config/nvim/lua/blues-nvim/plugins/core/nvim-treesetter.lua
+++ nvim/.config/nvim/lua/blues-nvim/plugins/core/nvim-treesetter.lua
@@ -1,51 +1,56 @@
+local parsers = {
+	"vim",
+	"vimdoc",
+	"lua",
+	"query",
+	"python",
+	"go",
+	"javascript",
+	"typescript",
+	"sql",
+	"bash",
+	"markdown",
+	"markdown_inline",
+	"json",
+	"yaml",
+	"toml",
+	"html",
+	"css",
+	"git_config",
+	"git_rebase",
+	"gitcommit",
+	"gitignore",
+	"gitattributes",
+	"csv",
+	"dockerfile",
+	"terraform",
+	"diff",
+	"just",
+	"regex",
+}
+
 return {
 	"nvim-treesitter/nvim-treesitter",
+	branch = "main",
+	lazy = false,
 	build = ":TSUpdate",
 	config = function()
-		local configs = require("nvim-treesitter.configs")
+		require("nvim-treesitter").setup()
 
-		configs.setup({
-			ensure_installed = {
-				-- Essential
-				"vim",
-				"vimdoc",
-				"lua",
-				"query",
-				-- Actively used languages
-				"python",
-				"go",
-				"javascript",
-				"typescript",
-				"sql",
-				"bash",
-				-- Markup/Config
-				"markdown",
-				"json",
-				"yaml",
-				"toml",
-				"html",
-				"css",
-				-- Git
-				"git_config",
-				"git_rebase",
-				"gitcommit",
-				"gitignore",
-				"gitattributes",
-				-- Data/DevOps
-				"csv",
-				"dockerfile",
-				"terraform",
-				-- Note-taking
-				"norg",
-				-- Other
-				"diff",
-				"just",
-				"regex",
-			},
-			sync_install = false,
-			auto_install = true,
-			highlight = { enable = true, additional_vim_regex_highlighting = false },
-			indent = { enable = true },
+		-- Install any missing parsers from the list above
+		local installed = require("nvim-treesitter").installed()
+		local missing = vim.tbl_filter(function(p)
+			return not vim.tbl_contains(installed, p)
+		end, parsers)
+		if #missing > 0 then
+			require("nvim-treesitter").install(missing)
+		end
+
+		vim.api.nvim_create_autocmd("FileType", {
+			callback = function()
+				pcall(vim.treesitter.start)
+				vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+			end,
 		})
 	end,
 }
